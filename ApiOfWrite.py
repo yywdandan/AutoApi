d2e6ef8842b76b33df6c9a0b5e0c5022d34e384c
yywdandan <74121994+yywdandan@users.noreply.github.com>
2021-03-01 09:37:09 +0800

    Update ApiOfWrite.py

--- ApiOfWrite.py
+++ ApiOfWrite.py
@@ -279,7 +279,7 @@ for a in range(0, app_count):
     print('账号 '+str(a+1))
     print('发送邮件 ( 邮箱单独运行，每次运行只发送一次，防止封号 )')
     if other_config['email'][0] != '':
-        sendEmail(a,'weather',weather)
+        #sendEmail(a,'weather',weather)
     else:
         print("尚未配置邮箱")
 print('')
